22093fe5ed7284e4b31c2c52ae33b0be5bbdf4c5
victor_naidu <victor@trillo.io>
2018-06-02 06:28:44 -0700

    as

---
+++ TaskManager/static/js/TaskListC.js
@@ -0,0 +1,31 @@
+/* globals TaskManager */
+
+(function() {
+
+  TaskManager.TaskListC = Trillo.inherits(Shared.SharedC, function(viewSpec) {
+    Shared.SharedC.call(this, viewSpec);
+  });
+
+  var TaskListC = TaskManager.TaskListC.prototype;
+  var SharedC = Shared.SharedC.prototype;
+
+  TaskListC.handleAction = function(actionName, selectedObj, $e, targetController) {
+    if (actionName === "edit") {
+      this.showForm(selectedObj);
+      return this.getData();
+    }
+    return SharedC.handleAction.call(this, actionName, selectedObj, $e, targetController);
+  };
+
+  TaskListC.showForm = function showForm(selectedObj) {
+    this.showView({
+      name: "EditTask",
+      container: 'trillo-dialog2-container',
+      data: selectedObj
+    });
+  };
+
+  TaskListC.postViewShown = function(view) {
+    SharedC.postViewShown.call(this, view);
+  };
+})();
